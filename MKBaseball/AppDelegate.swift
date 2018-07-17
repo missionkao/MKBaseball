eaee4ceff05178f65e16fe1433ff15890cfbfc18
Mission <kaominghsuan@gmail.com>
2018-07-17 23:24:31 +0800

    delete Main.storyboard and add MKIndexViewController as rootViewController

--- MKBaseball/AppDelegate.swift
+++ MKBaseball/AppDelegate.swift
@@ -13,9 +13,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        if let window = self.window {
+            window.rootViewController = MKIndexViewController()
+            window.makeKeyAndVisible()
+        }
+        
         return true
     }
 
---
+++ MKBaseball/MKIndexViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MKIndexViewController.swift
+//  MKBaseball
+//
+//  Created by Mission on 2018/7/17.
+//  Copyright © 2018年 Mission. All rights reserved.
+//
+
+import UIKit
+
+class MKIndexViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
