e702e3d6616a65f6aed13294a8aeeb9c0e6d0974
Mason Kao <kaominghsuan@gmail.com>
2018-08-04 13:50:53 +0800

    viewModel

---
+++ MKBaseball/MKTodayViewController/MKTodayViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  MKTodayViewModel.swift
+//  MKBaseball
+//
+//  Created by Mason Kao on 2018/8/4.
+//  Copyright © 2018年 Mission. All rights reserved.
+//
+
+
+class MKTodayViewModel {
+    func todayGameCount() -> Int {
+        return 2
+    }
+}
